e60fb59647b32f3c054a01349db568103351c478
MICHEAL SHODAMOLA <micheal.shodamola@centria.fi>
2023-11-05 19:43:02 +0200

    Add project files.

---
+++ AbstractClassDemo/InventoryItem.cs
@@ -0,0 +1,11 @@
+﻿namespace AbstractClassDemo
+{
+    //Abstract classes are class you build upon(Base Clase, foundation)
+    // you cant directly instantiate an abstract class
+    public abstract class InventoryItem
+    {
+
+        public string productName { get; set; }
+        public int QuantiryonHand { get; set; }
+    }
+}
---
+++ AbstractClassDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InventoryItem item = new Book();
+
+
+            item.productName = "Huh?";
+            
+            vehicle Corolla = new Car();
+
+            Car corolla = new Car();
+
+            corolla.NumberOfwheels = 4;
+
+            Console.ReadLine();
+        }
+    }
+
+    // Abstract
+    public abstract class vehicle 
+    {
+        // Methods
+        public string VINNumber { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+
+        public int YearofManufacturer { get; set; }
+
+    }
+
+    // Class inheriting from Abstract class vehicle
+    public class Car : vehicle
+    {
+        public int NumberOfwheels { get; set; } = 4;
+
+    }
+}
